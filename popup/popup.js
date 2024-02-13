2685452fc2034f1b8f9c0df7ff5641733e21934b
JohanGallardo <jsg6293@nyu.edu>
2024-02-13 18:51:10 -0500

    added message listener to fix time elapsed to use true start time

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,14 @@
 let totalClicks = 0;
 
 function updateStats(startTime) {
-    let timeElapsed = Math.floor((Date.now() - startTime) / 1000);
+    //let timeElapsed = Math.floor((Date.now() - startTime) / 1000);
     document.getElementById('clicks').textContent = totalClicks;
-    document.getElementById('time').textContent = timeElapsed;
+    //document.getElementById('time').textContent = timeElapsed;
     document.getElementById('scroll').textContent = Math.floor(scrollDistance);
+    browser.runtime.sendMessage({command: "getTimeSpent"}).then(response => {
+        const timeSpent = response.timeSpent;
+        document.getElementById("time").textContent =Math.floor(timeSpent / 1000);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
